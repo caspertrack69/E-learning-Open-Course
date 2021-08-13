6f962eff78de76de8dc85371f7a60e73ac3f7788
DESKTOP-IN7C8M5\ASUS PC <r.tullah767@gmail.com>
2021-08-13 13:04:14 +0700

    tambah login berhasil

--- app/src/main/java/com/example/TeacherApp/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/example/TeacherApp/ui/logout/LogoutFragment.java
@@ -1,8 +1,12 @@
 package com.example.TeacherApp.ui.logout;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -13,27 +17,27 @@ import com.example.TeacherApp.DatabaseHelper;
 import com.example.TeacherApp.LoginActivity;
 import com.example.TeacherApp.R;
 
-public class LogoutFragment extends AppCompatActivity {
+public class LogoutFragment extends Fragment {
     DatabaseHelper db;
     Button logout;
 
-    int tambah;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_logout);
+//        setContentView(R.layout.fragment_logout);
+        View view = inflater.inflate(R.layout.fragment_logout, container, false);
+        db = new DatabaseHelper(getActivity());
 
-        db = new DatabaseHelper(this);
-
-        logout = (Button)findViewById(R.id.btn_logout);
+        logout = (Button) view.findViewById(R.id.btn_logout);
 
 
         Boolean checkSession = db.checkSession("ada");
         if (checkSession == false) {
-            Intent loginIntent = new Intent(LogoutFragment.this, LoginActivity.class);
+            Intent loginIntent = new Intent(getActivity(), LoginActivity.class);
             startActivity(loginIntent);
-            finish();
+            onStop();
         }
 
         // logout
@@ -42,12 +46,13 @@ public class LogoutFragment extends AppCompatActivity {
             public void onClick(View v) {
                 Boolean updtSession = db.upgradeSession("kosong", 1);
                 if (updtSession == true) {
-                    Toast.makeText(getApplicationContext(), "Berhasil Keluar", Toast.LENGTH_SHORT).show();
-                    Intent loginIntent = new Intent(LogoutFragment.this, LoginActivity.class);
+                    Toast.makeText(getActivity().getApplicationContext(), "Berhasil Keluar", Toast.LENGTH_SHORT).show();
+                    Intent loginIntent = new Intent(getActivity(), LoginActivity.class);
                     startActivity(loginIntent);
-                    finish();
+                    onStop();
                 }
             }
         });
+        return view;
     }
 }
